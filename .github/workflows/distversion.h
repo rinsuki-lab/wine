78a41685bc267fd09a961d272990e29493b91e31
rinsuki <428rinsuki+git@gmail.com>
2021-05-20 20:07:35 +0900

    build w/ github actions

---
+++ .github/workflows/distversion.h
@@ -0,0 +1,14 @@
+/* ---------------------------------------------------------------
+ *   distversion.c
+ *
+ * Copyright 2013, CodeWeavers, Inc.
+ * 
+ * Information from DISTVERSION which needs to find 
+ * its way into the wine tree.
+ * --------------------------------------------------------------- */
+// modified by rinsuki
+// from https://github.com/PhoenicisOrg/winecx/blob/ee369bd8c8b927aa3c8bce9a75a877225375f3ab/include/distversion.h
+
+#define WINDEBUG_WHAT_HAPPENED_MESSAGE "This can be caused by a problem in the program or a deficiency in Wine. You may want to check <a href=\"http://www.codeweavers.com/compatibility/\">http://www.codeweavers.com/compatibility/</a> for tips about running this application."
+
+#define WINDEBUG_USER_SUGGESTION_MESSAGE "If this problem is not present under Windows and has not been reported yet, you can save the detailed information to a file using the \"Save As\" button, then let's debug time!"
