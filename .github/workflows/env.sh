e2c2089cd1e8020f1409298e3994699573bf4e1b
rinsuki <428rinsuki+git@gmail.com>
2021-05-21 19:12:55 +0900

    ci: decrase deployment target to 10.14

--- .github/workflows/env.sh
+++ .github/workflows/env.sh
@@ -3,4 +3,4 @@ export PATH="/opt/cwbgh/llvm/bin:/usr/local/opt/bison/bin:$PATH"
 export CROSSCFLAGS="-g -O2 -fcommon"
 export CC="clang"
 export CXX="clang++"
-export MACOSX_DEPLOYMENT_TARGET=10.15
+export MACOSX_DEPLOYMENT_TARGET=10.14
